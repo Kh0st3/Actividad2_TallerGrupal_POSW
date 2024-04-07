de2126f52df93463e4e18e0937154eb0c4cd13f7
Ricardo Caseres Ch <ricardocc5@hotmail.com>
2024-04-07 18:17:46 -0500

    Se agrega el proyecto

---
+++ db.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+
+$conn = mysqli_connect(
+    'localhost',
+    'root',
+    '',
+    'task_php_mysql'
+);
+
+/* Codigo para comprobar conexion
+if (isset($conn)) {
+    echo 'DB is connected';
+}
+*/
+
+
+
+
+?>
---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TASK PHP MYSQL</title>
+    <!--bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!--fontawesome-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
+</head>
+<body>
+
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <a href="index.php" class="navbar-brand">TASK PHP MYSQL</a>
+    </div>
+</nav>
